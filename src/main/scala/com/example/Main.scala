e63158aa31dff16cf185a26e28deafbfcbaba706
Leif Battermann <leif.battermann@gmail.com>
2019-01-11 00:43:40 +0100

    nested mcs

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -54,19 +54,30 @@ object Search {
         }
     }
 
-  def simpleMonteCarlo[F[_]: Monad, Move, Position, Score](game: Game[F, Move, Position, Score])(implicit ord: Ordering[Score]): F[Unit] = {
+  def nestedMonteCarlo[F[_]: Monad, Move, Position, Score](level: Int, game: Game[F, Move, Position, Score])(implicit ord: Ordering[Score]): F[Unit] = {
     val playMoveWithBestSimulationResult = for {
-      moves        <- game.legalMoves
+      legalMoves   <- game.legalMoves
       currentState <- game.gameState
-      isTerminalPosition <- moves match {
+      isTerminalPosition <- legalMoves match {
         case Nil => game.pure(true)
-        case ms =>
-          ms.traverse { m =>
+        case moves =>
+          val results = if (level <= 1) {
+            moves.traverse { move =>
               for {
-                nextState <- game.update(_.copy(gameState = currentState)) *> game.applyMove(m) *> game.gameState
+                nextState <- game.update(_.copy(gameState = currentState)) *> game.applyMove(move) *> game.gameState
                 simResult <- game.rndSimulation *> game.gameState
               } yield (simResult, nextState)
             }
+
+          } else {
+            moves.traverse { m =>
+              for {
+                nextState <- game.update(_.copy(gameState = currentState)) *> game.applyMove(m) *> game.gameState
+                simResult <- nestedMonteCarlo(level - 1, game) *> game.gameState
+              } yield (simResult, nextState)
+            }
+          }
+          results
             .map(_.maxBy(_._1.score))
             .flatMap { case (simResult, nextState) => game.update(updateBestResultAndSetNextState(simResult, nextState)).as(false) }
       }
@@ -77,9 +88,9 @@ object Search {
 }
 
 object Main extends IOApp {
-  private val game  = data.Games.game1
+  private val game  = data.Games.board(7)
   private val score = SameGame.score(game)
-  private val s     = SearchState(Seed(5L), GameState(List.empty[samegame.Position], score, game), None)
+  private val s     = SearchState(Seed(50L), GameState(List.empty[samegame.Position], score, game), None)
 
   private implicit val showResult: Show[Option[Result[samegame.Position, Int]]] = Instances.showResult
 
@@ -87,17 +98,17 @@ object Main extends IOApp {
 
   val resultState: IO[Unit] = {
     val instance = Instances.withState()
-    val result   = Search.simpleMonteCarlo(instance).runS(s).value.bestResult
+    val result   = Search.nestedMonteCarlo(3, instance).runS(s).value.bestResult
     putStrLn(result)
   }
 
   val resultIORef: IO[Unit] = for {
     instance <- Instances.withIORef(s)
-    result   <- Search.simpleMonteCarlo(instance) *> instance.bestResult
+    result   <- Search.nestedMonteCarlo(2, instance) *> instance.bestResult
     _        <- putStrLn(result)
   } yield ()
 
   def run(args: List[String]): IO[ExitCode] =
-    // resultIORef.as(ExitCode.Success)
+    //resultIORef.as(ExitCode.Success)
     resultState.as(ExitCode.Success)
 }
