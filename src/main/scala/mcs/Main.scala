e0433eeee34f3e3876fbc9ac7c53a1ee59f838df
Leif Battermann <leifbattermann@gmail.com>
2019-02-07 22:09:02 +0100

    cached thread pool

--- src/main/scala/mcs/Main.scala
+++ src/main/scala/mcs/Main.scala
@@ -1,16 +1,18 @@
 package mcs
 
 import cats.effect.concurrent.Ref
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{ContextShift, ExitCode, IO, IOApp}
 import cats.implicits._
 import mcs.Interpreters._
 import mcs.Programs.SearchState
 import mcs.samegame.SameGame
-
 import scala.util.Try
 
 object Main extends IOApp {
-  private val (position, _) = data.Games.board(15)
+  private val es                                                 = java.util.concurrent.Executors.newCachedThreadPool()
+  override protected implicit def contextShift: ContextShift[IO] = IO.contextShift(scala.concurrent.ExecutionContext.fromExecutor(es))
+
+  private val (position, b) = data.Games.jsGames10
   private val score         = SameGame.score(position)
   private val gameState     = GameState(playedMoves = List.empty[Move], score = score, position = position)
 
@@ -20,7 +22,8 @@ object Main extends IOApp {
       _     <- IO(println(s"Available processors: $cores"))
       _     <- IO(println(s"Nesting level: $level"))
       ref   <- Ref.of[IO, Option[Result[Move, Int]]](None)
-      _     <- Programs.nestedMonteCarlo[IO, IO.Par, Move, BoardPosition, Int](SearchState(gameState, None), ref, level)
+      _     <- Programs.nestedMonteCarlo[IO, IO.Par, Move, BoardPosition, Int](SearchState(gameState, b), ref, level)
+      _     <- IO(es.shutdown())
     } yield ()
   }
 
